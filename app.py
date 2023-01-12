db7c3b05790b6eae0f9be94b8fc0c85a3ed3245b
mrish08 <muyright22@gmail.com>
2023-01-13 03:39:20 +0800

    s

--- app.py
+++ app.py
@@ -277,15 +277,13 @@ def clinicadmin():
 	
 	
 
-@app.route("/addcs")
+@app.route("/addcs", methods = ['GET', 'POST'])
 def addcs():
 	if request.method == 'POST':
 		clinic_services_name= request.form['clinic_services_name']
-		
 	conn = connection()
 	cursor = conn.cursor()
-	cursor.execute('INSERT INTO ih_clinic_services(clinic_services_name)'' VALUES (%s)', 
-	[clinic_services_name])
+	cursor.execute('INSERT INTO ih_clinic_services (clinic_services_name)'' VALUES (%s)',[clinic_services_name])
 	conn.commit()
 	conn.close()
 	return render_template("admin-add-clinicservice.html")
