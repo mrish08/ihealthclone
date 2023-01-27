5c6fd42a8f02f6820c2523c87162c6f7c8b18b3c
mrish08 <muyright22@gmail.com>
2023-01-27 19:31:01 +0800

    updated sql

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('database-1.c8punsklsimv.ap-southeast-1.rds.amazonaws.com', username="postgres", password="wew123WEW") 
 def insertUser(email,password):
-    con = sql.connect("bms.db")
+    con = sql.connect("database-1.c8punsklsimv.ap-southeast-1.rds.amazonaws.com")
     cur = con.cursor()
     cur.execute("INSERT INTO login_ihealth (email,password) VALUES (?,?)", (email,password))
     con.commit()
