d13c10d99028fabdfd2f6c57d22a3e85c2185771
mrish08 <muyright22@gmail.com>
2023-02-13 12:22:09 +0800

    up

--- app.py
+++ app.py
@@ -581,5 +581,4 @@ def reshistoryviewclinic():
 	return render_template("reshistory-view-clinic.html")
 
 if __name__== '__main__':
-	app.secret_key = os.urandom(12)	
-	app.run(host="0.0.0.0",port=8000)
+	app.run()
