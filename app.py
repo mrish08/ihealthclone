7e3e86d5029621bf38bac06c589048ffdd0983f1
mrish08 <muyright22@gmail.com>
2023-02-18 00:19:32 +0800

    update app

--- app.py
+++ app.py
@@ -390,7 +390,14 @@ def adminhaptmedicine():
 
 @app.route("/adminhaptclinic")
 def adminhaptclinic():
-	return render_template("adminhistory-apt-clinic.html")
+	adminhaptclinic=[]
+	conn = connection()
+	cursor = conn.cursor()
+	cursor.execute("SELECT IH.APPT_TYPE,IH.DATE, IH.STATUS, U.FIRSTNAME, CSV.CLINIC_SERVICES_NAME, CSH.SCHEDULE_NAME FROM IH_APPOINTMENT IH INNER JOIN IH_CLINIC_SERVICES CSV ON IH.CLINIC_SERVICES_ID = CSV.CLINIC_SERVICES_ID INNER JOIN IH_CLINIC_SCHED CSH ON CSH.CLINIC_SCHED_ID = IH.CLINIC_SCHED_ID INNER JOIN USERS_USER U ON U.ID = IH.ID ")		
+	for row in cursor.fetchall():
+			adminhaptclinic.append({ "appt_type": row[0],"date": row[1],"status": row[2],"firstname": row[3],"clinic_services_name": row[4],"schedule_name": row[5]})
+	conn.close()
+	return render_template("adminhistory-apt-clinic.html",adminhaptclinic=adminhaptclinic)
 
 
 @app.route("/indexstaff")
@@ -514,6 +521,7 @@ def staffhviewclinic():
 
 @app.route("/indexresident")
 def indexresident():
+	""""
 	email= "kylemara@gmail.com" 
 	account_type= "Resident"
 	conn = connection()
@@ -522,8 +530,11 @@ def indexresident():
 	user = cursor.fetchone()
 	if user is not None:
 		user_id, user_firstname = user
-		session['user_id'] = user_id
-		session['user_firstname'] = user_firstname
+		"""
+	if('user_id' in session):
+		user_id = session['user_id']
+		user_firstname=session['user_firstname'] 
+
 		return render_template("indexresident.html")
 
 @app.route("/scheduleresident")
